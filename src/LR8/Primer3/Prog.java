e2388c1b0d304b3e8af22d912eb7ee7204d3ecb8
1Denzo <denis_zotov@inbox.ru>
2023-01-17 18:27:30 +0500

    LR8

---
+++ src/LR8/Primer3/Prog.java
@@ -0,0 +1,23 @@
+package LR8.Primer3;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static java.lang.System.out;
+
+public class Prog {
+    public static void main(String[] args) {
+        try {
+            var fileName = "C:\\temp\\text.txt";
+            InputStream inFile = null;
+            try {
+                inFile = new FileInputStream("C:\\temp\\text.txt");
+                Reader.readAllByByteToConsole(inFile);
+            } finally {
+                if (inFile != null) inFile.close();
+            }
+        } catch (Exception e) {
+            out.printf("Error - %e", e);
+        }
+    }
+}

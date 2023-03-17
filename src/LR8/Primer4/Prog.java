97e48d3703347120c958493c12e515e4209676ad
denzo <denis_zotov@inbox.ru>
2023-03-17 10:44:23 +0500

    LR8 rebild 4/12-2

--- src/LR8/Primer4/Prog.java
+++ src/LR8/Primer4/Prog.java
@@ -5,28 +5,33 @@ import java.util.Scanner;
 
 import static java.lang.System.out;
 import static java.lang.System.in;
+/* Программу Primer4 составить в соответствии со следующим алгоритмом:
+1. Создать новую папку My на диске.
+2. В папке создать 1-й файл numIsh.txt.
+3. В файл записать через поток числа, вводимые с клавиатуры.
+4. В той же папке создать 2-й файл numRez.txt.
+5.1 Открыть 1-й поток для чтения из 1-го файла numIsh.txt;
+5.2 Открыть 2-й поток для записи во 2-й файл numRez.txt;
+5.3 Пока не конец 1-го файла:
+а) Считать число из 1-го потока (файла numIsh.txt);
+б) Записать число во 2-й поток (файл numRez.txt);
+в) Вывести число на экран;
+5.4 Дописать остаток данных на диск; 5.5)
+закрыть 1-й поток;
+5.6 Закрыть 2-й поток.
+6. Закончить.
+*/
 
-public class Prog {
-    public static void main(String[] args) throws Exception {
+public class Primer4 {
+    public static void main(String[] args) {
         try {
-            var f1 = new File("C:\\temp\\numIsh.txt");
+            //Создание исходного файла numIsh.txt и записать в него число типа float
+            File f1 = new File("D:\\My\\numIsh.txt");
             f1.createNewFile();
-            var scan = new Scanner(in, "UTF-8");
-            out.print("Сколько вещественных чисел записать в файл?: ");
-            int count = scan.nextInt();
-            float[] arr = new float[count];
-            out.print("Введите числа: ");
-            for (int i = 0; i < count; i++)
-                arr[i] = scan.nextFloat();
-            FSWork.writeFloatsToFile(f1.getAbsolutePath(), arr);
+            Scanner sc = new Scanner(System.in, "cp1251");
+            DataOutputStream wr = new DataOutputStream(new FileOutputStream(f1.getAbsolutePath()));
 
-            var f2 = new File("C:\\temp\\numRez.txt");
-            f2.createNewFile();
-            FSWork.copyFile(f1.getAbsolutePath(), f2.getAbsolutePath());
-        } catch (Exception e) {
-            out.printf("Error - %e", e);
         }
     }
 }
-
 

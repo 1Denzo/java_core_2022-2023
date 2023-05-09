26324964fc697db7c8aa403318345ac614407dd0
1Denzo <denis_zotov@inbox.ru>
2023-05-09 23:33:44 +0500

    LR10 ParseXML constructor

--- src/LR10/Example1XML/DOMxmlWriter.java
+++ src/LR10/Example1XML/DOMxmlWriter.java
@@ -1,6 +1,7 @@
 package LR10.Example1XML;
 
-    import java.io.File;
+import java.io.File;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -13,72 +14,91 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import java.time.LocalDate;
 // https://javadevblog.com/kak-sozdat-xml-fajl-na-java-ispol-zuem-dom-parser.html
-    public class DOMxmlWriter {
+public class DOMxmlWriter {
 
-        public static void main(String[] args) {
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder;
-            try {
-                builder = factory.newDocumentBuilder();
+    public static void main(String[] args) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        Scanner in = new Scanner(System.in);
+        String inVend, inModel, inAgeS = null;
+        int inAgeI = 0;
+        String k = null;
+        LocalDate date = LocalDate.now(); // получаем текущую дату
+        int year = date.getYear();
 
-                // создаем пустой объект Document, в котором будем
-                // создавать наш xml-файл
-                Document doc = builder.newDocument();
-                // создаем корневой элемент
-                Element rootElement =
-                        doc.createElement("Motobikes");
-                // добавляем корневой элемент в объект Document
-                doc.appendChild(rootElement);
+        try {
+            builder = factory.newDocumentBuilder();
+
+            // создаем пустой объект Document, в котором будем
+            // создавать наш xml-файл
+            Document doc = builder.newDocument();
+            // создаем корневой элемент
+            Element rootElement =
+                    doc.createElement("Motobike");
+            // добавляем корневой элемент в объект Document
+            doc.appendChild(rootElement);
+            System.out.println("Введите количество элементов списка " + rootElement.getTagName()+ ":");
+            int elementNumber = in.nextInt();
+            for (int i = 0; i < elementNumber; i++) {
+                k = String.valueOf(i + 1);
+                System.out.println("Введите название фирмы-изготовителя мотоцикла " + k + ":");
+                inVend = in.next();
+                System.out.println("Введите название модели мотоцикла " + k + ":");
+                inModel = in.next();
+                System.out.println("Введите год выпуска мотоцикла " + k + ":");
+                inAgeI = in.nextInt();
+                inAgeS = String.valueOf(year-inAgeI);
 
                 // добавляем первый дочерний элемент к корневому
-                rootElement.appendChild(getMotobike(doc, "Honda", "TransAlp", "15"));
-
-                //добавляем второй дочерний элемент к корневому
-                rootElement.appendChild(getMotobike(doc, "Kawasaki", "Ninja", "17"));
-
-                //создаем объект TransformerFactory для печати в консоль
-                TransformerFactory transformerFactory = TransformerFactory.newInstance();
-                Transformer transformer = transformerFactory.newTransformer();
-                // для красивого вывода в консоль
-                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-                DOMSource source = new DOMSource(doc);
-
-                //печатаем в консоль или файл
-                StreamResult console = new StreamResult(System.out);
-                StreamResult file = new StreamResult(new File("C:\\Users\\reus\\IdeaProjects\\java_core_2022-2023\\src\\LR10\\Example1XML\\Motobike1.xml"));
-
-                //записываем данные
-                transformer.transform(source, console);
-                transformer.transform(source, file);
-                System.out.println("Создание XML файла закончено");
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                rootElement.appendChild(getMotobike(doc, k, inVend, inModel, inAgeS));
             }
+            //создаем объект TransformerFactory для печати в консоль
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            // для красивого вывода в консоль
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            DOMSource source = new DOMSource(doc);
+
+            //печатаем в консоль или файл
+            StreamResult console = new StreamResult(System.out);
+            StreamResult file = new StreamResult(new File("src/LR10/Example1XML/motobike.xml"));
+
+            //записываем данные
+            transformer.transform(source, console);
+            transformer.transform(source, file);
+            System.out.println("Создание XML файла закончено");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        // метод для создания нового узла XML-файла
-        private static Node getMotobike(Document doc, String vendor,String model, String age) {
-            Element Motobike = doc.createElement("Motobike");
-
-            // создаем элемент Vendor
-            Motobike.appendChild(getLanguageElements(doc, "Vendor", vendor));
-
-            // создаем элемент Model
-            Motobike.appendChild(getLanguageElements(doc, "Model", model));
-
-            // создаем элемент age
-            Motobike.appendChild(getLanguageElements(doc, "age", age));
-            return Motobike;
-        }
-
-
-        // утилитный метод для создание нового узла XML-файла
-        private static Node getLanguageElements(Document doc, String name, String value) {
-            Element node = doc.createElement(name);
-            node.appendChild(doc.createTextNode(value));
-            return node;
-        }
-
     }
+
+    // метод для создания нового узла XML-файла
+    private static Node getMotobike(Document doc, String id, String vendor,String model, String age) {
+        Element Motobike = doc.createElement("Motobike");
+
+        // устанавливаем атрибут id
+        Motobike.setAttribute("id", id);
+
+        // создаем элемент Vendor
+        Motobike.appendChild(getMotobikeElements(doc, Motobike, "Vendor", vendor));
+
+        // создаем элемент Model
+        Motobike.appendChild(getMotobikeElements(doc, Motobike, "Model", model));
+
+        // создаем элемент age
+        Motobike.appendChild(getMotobikeElements(doc, Motobike, "age", age));
+        return Motobike;
+    }
+
+
+    // утилитный метод для создания нового узла XML-файла
+    private static Node getMotobikeElements(Document doc, Element element, String name, String value) {
+        Element node = doc.createElement(name);
+        node.appendChild(doc.createTextNode(value));
+        return node;
+    }
+
+}
---
+++ src/LR10/Example1XML/ParcerXML/Motobikes.java
@@ -0,0 +1,40 @@
+package LR10.Example1XML.ParcerXML;
+
+import org.w3c.dom.NodeList;
+
+import java.util.List;
+
+public class Motobikes {
+    private String name;
+    private int id;
+    private List<Motobike> motobikes;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMotobikes(List<Motobike> motobikes) {
+        this.motobikes = motobikes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Motobike> getMotobikes() {
+        return motobikes;
+    }
+
+    @Override
+    public String toString() {
+        return "Motobikes{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", motobikes=" + motobikes +
+                '}';
+    }
+}

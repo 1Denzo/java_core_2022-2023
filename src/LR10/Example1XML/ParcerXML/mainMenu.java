0da830eb2193f85c8ab01be9331e99a0ed852cc6
1Denzo <denis_zotov@inbox.ru>
2023-06-08 00:31:38 +0500

    LR 10 ParcerXML-JSON v0.9 3/4 of SearchOption

--- src/LR10/Example1XML/ParcerXML/mainMenu.java
+++ src/LR10/Example1XML/ParcerXML/mainMenu.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class mainMenu {
-    Scanner in = new Scanner(System.in);
-    String twoChoice1, twoChoice2 = "";
+        String twoChoice1, twoChoice2 = "";
     boolean checker = false;
 
     public String Menu() throws IOException {
         String choice;
+        Scanner in = new Scanner(System.in);
         //Runtime runtime = getRuntime();
         //Process process = runtime.exec("cls"); //для linux "clear", в виндовс "cls"
         System.out.print("\033[H\033[2J");
@@ -33,6 +33,7 @@ public class mainMenu {
         MotobikesList motobikesList = pars.parsMotoXML();
         System.out.println(motobikesList.toString());
         do {
+            Scanner in = new Scanner(System.in);
             System.out.println("Сохранить данные в файл .txt? (y/n)");
             oneChoice = in.nextLine();
             switch (oneChoice) {
@@ -49,6 +50,7 @@ public class mainMenu {
     public Byte Two1() {
         byte twoMenu = 0;
         do {
+            Scanner in = new Scanner(System.in);
             System.out.println("Поиск по полям Motobike: ");
             System.out.println("1. Поиск по полю id ");
             System.out.println("2. Поиск по полю Vendor ");
@@ -82,34 +84,57 @@ public class mainMenu {
     public short[] Two2() {
         short[] param = new short[2];
         do {
+            Scanner in = new Scanner(System.in);
             System.out.println("1. Поиск по значению (например Age = 5, только положительные целые числа) ");
-            System.out.println("2. Поиск по диапозону (напиример 3 < Age < 10)");
+            System.out.println("2. Поиск по диапозону (напиример 3 < Age < 10, только положительный диапазон)");
             System.out.println("Выберите один из пунктов: ");
             twoChoice2 = in.nextLine();
             switch (twoChoice2) {
                 case "1":
-                    System.out.println("Введите значение ");
-                    param[0] = in.nextShort();
+                        do {
+                            try {
+                                Scanner in2 = new Scanner(System.in);
+                        System.out.println("Введите значение ");
+                        param[0] = in2.nextShort();
+                    } catch (Exception e) {
+                        System.out.println("Ошибка ввода параметра! Пожайлуста, снова введите значение..." + e);
+                    }
                     if (param[0] > 0) {
                         checker = true;
-                    } else {
-                        System.out.println("Неверный диапазон значений!!! Пожайлуста снова введите значение...");
-                    }
+                    }}
+                    while (checker != true);
                     break;
                 case "2":
-                    System.out.println("Введите ограничение параметра поиска снизу (больше чем...): ");
-                    param[0] = in.nextShort();
-                    System.out.println("Введите ограничение параметра поиска сверху (меньше чем...): ");
-                    param[1] = in.nextShort();
-                    if (param[1] > param[0]) {
-                        checker = true;
-                    } else {
-                        System.out.println("Неверный диапазон значений!!! Пожайлуста снова введите значения...");
-                    }
+                    do {
+                        try {
+                            Scanner in3 = new Scanner(System.in);
+                            param[0] = 0;
+                            System.out.println("Введите ограничение параметра поиска снизу, целое положительное число (больше чем...): ");
+                            param[0] = in3.nextShort();
+                        } catch (Exception e) {
+                            System.out.println("Ошибка ввода параметра! Пожайлуста, снова введите значение..." + e);
+                        }
+                        try {
+                            Scanner in1 = new Scanner(System.in);
+                            param[1] = 0;
+                            System.out.println("Введите ограничение параметра поиска сверху, целое положительное число (меньше чем...): ");
+                            param[1] = in1.nextShort();
+                        } catch (Exception e) {
+                            System.out.println("Ошибка ввода параметра! Пожайлуста, снова введите значение..." + e);
+                        } if (param[0] > 0) {
+                            if (param[1] > 0) {
+                                if (param[1] > param[0]) {
+                                    checker = true;
+                                } else {
+                                    System.out.println("Неверный диапазон значений!!! Верхняя граница меньше нижней, пожайлуста снова введите значения границ диапазона.");
+                                }} else {
+                                System.out.println("Верхняя граница диапозона - не положительное число! Пожайлуста снова введите значения границ диапазона.");
+                            }} else {
+                            System.out.println("Нижняя граница диапозона - не положительное число! Пожайлуста снова введите значения границ диапазона.");
+                            }}
+                        while (checker != true);
                     break;
-            }
-        }
+        }}
         while (checker != true);
         return param;
-    }
-}
+}}

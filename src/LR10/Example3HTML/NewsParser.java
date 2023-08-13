b6b78fb6026da3413b958921d56b37463c43e32e
reus <zotov@met-trans.ru>
2023-08-13 18:52:21 +0500

    LR10 finish

--- src/LR10/Example3HTML/NewsParser.java
+++ src/LR10/Example3HTML/NewsParser.java
@@ -6,15 +6,32 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.dom.DOMSource;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class NewsParser {
-    public static void main(String[] args) {
-        try {
-            // Получаем HTML-код страницы
-            Document doc = Jsoup.connect("http://fat.urfu.ru/index.html").get();
+    private static final String TAG_URL = "http://fat.urfu.ru/index.html";
+    private static final String TAG_PATHFILE =
+            "C:\\Users\\reus\\IdeaProjects\\java_core_2022-2023\\src\\LR10\\Example3HTML\\";
 
+    public static void main(String[] args) {
+        String s, d = null;
+        Scanner sc = new Scanner(System.in, "UTF-8");
+        System.out.print("Введите имя файла для сохранения результатов парсинга URL: " + TAG_URL + "\n");
+        String fname = sc.nextLine();
+        try {
+            File fw = new File(TAG_PATHFILE + fname + ".txt");
+            fw.createNewFile(); // файл создан
+            System.out.println("Путь к файлу: " + fw.getAbsolutePath());
+            BufferedWriter dOut = new BufferedWriter(new FileWriter(fw));
+            // Получаем HTML-код страницы
+            Document doc = Jsoup.connect(TAG_URL).get();
             // Извлекаем список новостей
             Elements newsParent = doc.
                     select("body > table > tbody > tr > td > div > table > " +
@@ -24,15 +41,21 @@ public class NewsParser {
             for (int i = 3; i < 20; i++) {
                 if (!(i % 2 == 0)) {
                     List<Node> nodes = newsParent.get(0).childNodes();
-                    System.out.println("Тема: " + ((Element) nodes.get(i)).getElementsByClass("blocktitle").get(0).childNodes().get(0));
-
-                    System.out.println("Дата: " + ((Element) nodes.get(i)).getElementsByClass("blockdate").get(0).childNodes().get(0)
+                    s = ("Тема: " + ((Element) nodes.get(i)).getElementsByClass("blocktitle").get(0).childNodes().get(0));
+                    System.out.println(s);
+                    dOut.write(s + "\n");
+                    d = ("Дата: " + ((Element) nodes.get(i)).getElementsByClass("blockdate").get(0).childNodes().get(0)
                             + "\n");
+                    System.out.println(d);
+                    dOut.write(d + "\n");
                 }
             }
-            } catch (IOException e) {
+            System.out.println("Файл записан.");
+            dOut.flush();
+            dOut.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        }
-
+    }
 }
+

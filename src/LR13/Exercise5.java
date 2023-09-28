247ca0d9d5403d131ac8c62d43464ecbc0505b48
reus <zotov@met-trans.ru>
2023-09-28 09:46:20 +0500

    LR13 all done

--- src/LR13/Exercise5.java
+++ src/LR13/Exercise5.java
@@ -1,40 +1,51 @@
 package LR13;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.*;
+
 
 /*Напишите функцию, которая находит максимальный элемент в массиве целых чисел при помощи многопоточности.
  Количество потоков должно быть равно количеству ядер процессора.*/
 public class Exercise5 {
-    private static final int TAG_Core = 6;
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Scanner in = new Scanner(System.in);
+        Random random = new Random();
         System.out.println("Введите размер массива: ");
         int size = in.nextInt();
-        int threadQuotitu = size / TAG_Core;
-        for (int i = 1; i <= TAG_Core; i++){
-            Thread thread = new NumThread(i);
-            thread.start();
-            thread.join();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(size);
         }
+        int max = findMax(arr);
+        System.out.println("Максимальное значение в массиве: " + max);
+    }
+
+    public static int findMax(int[] arr) throws ExecutionException, InterruptedException {
+        int max = Integer.MIN_VALUE;
+        // Создаем пул потоков с фиксированным количеством потоков
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        // Создаем массив Future<Integer> для хранения результатов
+        Future<Integer>[] results = new Future[arr.length];
+
+        // Запускаем каждую задачу в отдельном потоке
+        for (int i = 0; i < arr.length; i++) {
+            final int index = i;
+            results[i] = executor.submit(() -> {
+                return arr[index];
+            });
+        }
+        // Ожидаем завершения всех задач и находим максимальное значение
+        for (Future<Integer> result : results) {
+            int value = result.get();
+            if (value > max) {
+                max = value;
+            }
+        }
+        // Завершаем пул потоков
+        executor.shutdown();
+        return max;
     }
 }
-class NumThread extends Thread {
-    private int number;
-    public NumThread(int number) {
-        this.number = number;
-    }
-    public void run() {
-        System.out.println("Поток " + number + " выполняется.");
-    }
-}
-    public static int[] Array = {5, 8, 34, 48, 2};
-    static public void Method1() // здесь вводится массив с данными
-    {
-        int sum = 0;
-        Max = Array[0];
-        Min = Array[0];
-        for (int j : Array) {
-            sum += j;
-            if (j > Max) Max = j;
-            if (j < Min) Min = j;
-}
+
--- src/LR13/Exercise6.java
+++ src/LR13/Exercise6.java
@@ -1,5 +1,53 @@
 package LR13;
 /*Напишите функцию, которая суммирует элементы в массиве целых чисел при помощи многопоточности.
  Количество потоков должно быть равно количеству ядер процессора.*/
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercise6 {
-}
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            Random random = new Random();
+            System.out.println("Введите размер массива: ");
+            int size = in.nextInt();
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = random.nextInt(size);
+            }
+            int sum = multithreadedSum(arr);
+            System.out.println(sum);
+        }
+
+        public static int multithreadedSum(int[] array) {
+            final int processors = Runtime.getRuntime().availableProcessors();
+            int chunkSize = array.length / processors;
+            final int[] sum = {0};
+
+            Thread[] threads = new Thread[processors];
+
+            for (int i = 0; i < processors; i++) {
+                final int start = i * chunkSize;
+                final int end = (i == processors - 1) ? array.length : (i + 1) * chunkSize;
+                threads[i] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int partialSum = 0;
+                        for (int j = start; j < end; j++) {
+                            partialSum += array[j];
+                        }
+                        sum[0] += partialSum;
+                    }
+                });
+                threads[i].start();
+            }
+            try {
+                for (Thread thread : threads) {
+                    thread.join();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return sum[0];
+        }
+    }
